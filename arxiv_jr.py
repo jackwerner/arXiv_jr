3858f63213c6df0684e267e369c8978cd9aec8ae
jackwerner <jckwern@aol.com>
2024-03-22 17:06:11 -0400

    Update arxiv_jr.py

--- arxiv_jr.py
+++ arxiv_jr.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 endpoint = "https://api.openai.com/v1/chat/completions" 
 api_key = st.secrets['OPENAI_KEY']
-model = "gpt-4-1106-preview"
+model = "gpt-4-0125-preview"
 openai = OpenAI(api_key=api_key)
 
 st.markdown("""
